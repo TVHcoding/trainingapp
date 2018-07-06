73dd79e6319dab5887944339aaa607a6f66f7e9f
Tim Van heester <timvanheestercoding@gmail.com>
2018-07-06 15:52:02 +0200

    Initial Commit

---
+++ Training app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Training app
+//
+//  Created by Tim Van heester on 6/07/18.
+//  Copyright © 2018 Tim Van heester. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
