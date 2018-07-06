9dcba5321c00d82912affdf38df9a35a7d40add9
Tim Van heester <timvanheestercoding@gmail.com>
2018-07-06 16:20:35 +0200

    werkende app optellen

--- Training app/ViewController.swift
+++ Training app/ViewController.swift
@@ -9,7 +9,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var coollabel: UILabel!
+    
+    @IBOutlet weak var text1: UITextField!
+    
+    @IBOutlet weak var text2: UITextField!
+    
+    @IBAction func button(_ sender: Any) {
+        
+        coollabel.text = "Het antwoord is \(Double(text1.text!)! + Double(text2.text!)!)"
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
